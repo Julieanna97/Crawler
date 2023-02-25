5c095e85ae8001b6c4a50e497af06945fbfcda2d
Julieanna97 <kisamae1997@gmail.com>
2023-02-25 20:39:41 +0100

    Not currently finished code

--- Python/crawler.py
+++ Python/crawler.py
@@ -11,7 +11,7 @@
 
 import os
 
-def main():
+def crawl():
 
     # Enter the file directory you want to search the string for
     #rootdir = ('C:\\Users\\Julie\\Documents\\GitHub\\Crawler\\TestData')
@@ -26,21 +26,19 @@ def main():
 
     # recursively walk through a folder and it's contents using os.walk()
     for folder, dirs, files in os.walk(cwd):
-        found = False
         for file in files:
             if file.endswith(extensions):
                 dir_path = os.path.join(folder, file)
                 with open(dir_path, 'r', encoding = "utf8") as f:
                     for line in f:
                         if search_str.lower() in line.lower():
-                            print("{} can be found at:\n-----------\n{}\n-----------".format(search_str, dir_path))
-                            found = True
+                            print("{} can be found at:\n{}".format(search_str, dir_path))
                             break
-
+                        
     if not os.path.isfile(dir_path):
-        print("Error! {} doesn't seem to be formattable...".format(dir_path))
+        print("Error: File doesn't exist")
 
-main()
 
-""" MORE FUNCTIONS COMING """
-""" CODE HERE """
+if __name__ == '__main__':
+
+    crawl()
