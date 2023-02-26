301919e0d3a22631edd32422c8e98771053074a4
Julieanna97 <kisamae1997@gmail.com>
2023-02-26 13:19:15 +0100

    file crawl

--- Python/crawler.py
+++ Python/crawler.py
@@ -2,50 +2,63 @@
 # Filename: crawler.py
 # Author: Julie Anne Cantillep (julie.cantillep@studerande.movant.se)
 # Purpose: Simple find string in files (recursive) script
+#          Store file address as values to keywords[key] in a hash map
 # Usage: Search for string of characters in subdirectories incl. 'TestData' folder
+#        by keywords
 #        [File Filters(ex/default: ".txt", ".md", ".docx")]
 # Requirements: Files must be text
 # WARNING: None
-#
+# Version: 1.0
+# Date: 2023-02-26
 #------------------------------------------------------
 
 import os
 
+
 class HashMap:
-        def __init__(self):
-                self.size = 6
-                self.map = [None] * self.size
-		
-        def _get_hash(self, key):
+        """ Hash map which uses strings for keys. Value can be any object. 
+        Provides optional map operations, and searches for values by keywords."""
+
+        def __init__(self, capacity = 1000):
+                """ Capacity defaults to 1000. """
+
+                # construct start at 0
+                # initialized hash table of size
+                self.capacity = capacity
+                self.size = 10               
+                self.map = [None] * self.size     
+
+        def get_hash(self, key):
+                """ computes hash codes using mod % 
+                to assign keys to specific hash table index """
+
                 hash = 0
+                # calculate hash code for every key
                 for char in str(key):
                         hash += ord(char)
                 return hash % self.size
-		
+	
         def add(self, key, value):
-                key_hash = self._get_hash(key)
-                key_value = [key, value]
+                """ Adds key-value pair into hash table. If key already exists, then the object will be
+                updated. If the key has multiple values, it will associate multiple values
+                with that key """
+
+                key_hash = self.get_hash(key)
+                key_val = [key, value]
 		
                 if self.map[key_hash] is None:
-                        self.map[key_hash] = list([key_value])
+                        self.map[key_hash] = list([key ,value])
                         return True
                 else:
                         for pair in self.map[key_hash]:
                                 if pair[0] == key:
                                         pair[1] = value
                                         return True
-                        self.map[key_hash].append(key_value)
+                        self.map[key_hash].append(key_val[1])
                         return True
-			
-        def get(self, key):
-                key_hash = self._get_hash(key)
-                if self.map[key_hash] is not None:
-                        for pair in self.map[key_hash]:
-                                if pair[0] == key:
-                                        return pair[1]
-                return None
-			
-        def delete(self, key):
+        
+        # delete function only for test (not working)
+        """def delete(self, key):
                 key_hash = self._get_hash(key)
 		
                 if self.map[key_hash] is None:
@@ -54,22 +67,29 @@ class HashMap:
                         if self.map[key_hash][i][0] == key:
                                 self.map[key_hash].pop(i)
                                 return True
-                return False
-	
+                return False"""
+        
         def keys(self):
+                """ prints key found during 'keyword' search """
+
                 arr = []
                 for i in range(0, len(self.map)):
                         if self.map[i]:
                                 arr.append(self.map[i][0])
                 return arr
-			
+		
         def print(self):
-                print('---HASH MAP----')
+                """ prints out the hash map """
+
+                print('----HASH MAP----')
                 for item in self.map:
                         if item is not None:
                                 print(str(item))
 
+
         def crawl(self):
+                """ search through files to find the keyword and store it in the hash table """
+
                # Enter the file directory you want to search the string for
                 #rootdir = ('C:\\Users\\Julie\\Documents\\GitHub\\Crawler\\TestData')
 
@@ -83,29 +103,30 @@ class HashMap:
 
                 # recursively walk through a folder and it's contents using os.walk()
                 for folder, dirs, files in os.walk(cwd):
-                    found = False
                     for file in files:
                         if file.endswith(extensions):
                             dir_path = os.path.join(folder, file)
                             with open(dir_path, 'r', encoding = "utf8") as f:
                                 for line in f:
+                                    # .lower() to implement case insensitivity during input and search
                                     if search_str.lower() in line.lower():
                                         print("{} can be found at:\n-----------\n{}\n-----------".format(search_str, dir_path))
                                         key, value = search_str, dir_path
                                         self.add(key, value)
-                                        found = True
                                         break
-
+                                    
+                # if file is invalid                             
                 if not os.path.isfile(dir_path):
                        print("CHECK THE ADDRESS FILE... Seems to be something wrong with the file directory!")
                 
 
-
+# Driver code
 if __name__ == '__main__':
+
         h = HashMap()
+        # crawl() searches for text of strings or characters in subdirs/cwd
         h.crawl()
-        h.print()		
-        """h.delete('Bob')
+        # prints the hash table
         h.print()
-        print('Ming: ' + h.get('Ming'))
-        print(h.keys())"""
+        print(h.keys())
+        input("Press ENTER to exit...")
